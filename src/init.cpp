7d1801af645d9a800dfe01ee4c1a2165ae0ff17a
Delukin Svyatoslav <svyatoslav.d@blaize.tech>
2019-09-25 17:12:37 +0300

    update copyright

--- src/init.cpp
+++ src/init.cpp
@@ -591,7 +591,9 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2015-%i The PIVX Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
-           FormatParagraph(strprintf(_("Copyright (C) 2017-%i The BITWIN24 Core Developers"), COPYRIGHT_YEAR)) + "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2017-2018 The MAC Core Developers"), COPYRIGHT_YEAR)) + "\n" +
+           "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2019 The BITWIN24 Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +

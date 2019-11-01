27ef10454f7abaa2c9018c49b2b58d0731ffcc2e
Svyatoslav Delukin <svyatoslav.d@blaize.tech>
2019-11-01 15:54:25 +0200

    skip time for pso mining

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -183,7 +183,7 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime = 1572411600;
+        genesis.nTime = 1572494400;
         genesis.nBits = 0x1e0ffff0;
         genesis.nNonce = 92133373;
 

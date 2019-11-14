e0f2df7b2adc188f98f6f6f5d11e65c749aabb24
Svyatoslav Delukin <svyatoslav.d@blaize.tech>
2019-11-14 08:26:10 +0200

    print log func

---
+++ src/trace-log.cpp
@@ -0,0 +1,11 @@
+//
+// Created by s on 13.11.2019.
+//
+
+#include "trace-log.h"
+
+
+int trace_call = 0;
+
+//loguru::init(argc, argv);
+//loguru::add_file("trace_log.log", loguru::Append, loguru::Verbosity_MAX);
---
+++ src/trace-log.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//#include "loguru.hpp"
+
+#include "util.h"
+
+extern int trace_call;
+
+//#define FUNC_LOG_TRACE() LOG_F(INFO, "I'm hungry for some %.3f!", 3.14159);
+#define FUNC_LOG_TRACE() LogPrintf("trace %S %d\n", __FUNCTION__, trace_call++)
+//std::string teststr = "hjyvgfjhvyj";
+//#define FUNC_LOG_TRACE LogPrintStr(teststr);
+//#define FUNC_LOG_TRACE ;

9c38e5fb0c36406d63546867db84080290f837c1
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2019-11-19 11:45:53 +0100

    Update bitwin24_autoinstall.sh

--- bitwin24_autoinstall.sh
+++ bitwin24_autoinstall.sh
@@ -43,15 +43,6 @@ function stop_daemon {
         fi
     fi
 }
-#Function detect_ubuntu
-
- if [[ $(lsb_release -d) == *16.04* ]]; then
-   UBUNTU_VERSION=16
-else
-   echo -e "${RED}You are not running Ubuntu 16.04, Installation is cancelled.${NC}"
-   exit 1
-
-fi
 
 #Process command line parameters
 genkey=$1

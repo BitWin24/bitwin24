b2e2caff2d79fd95d06d246b5905680bd7569a26
Svyatoslav Delukin <svyatoslav.d@blaize.tech>
2019-11-26 17:09:27 +0200

    print log to own file

--- src/trace-log.cpp
+++ src/trace-log.cpp
@@ -9,3 +9,17 @@ int trace_call = 0;
 
 //loguru::init(argc, argv);
 //loguru::add_file("trace_log.log", loguru::Append, loguru::Verbosity_MAX);
+
+
+void WriteToFile(const char* function)
+{
+    char filename[256];
+    sprintf(filename, "/home/s/my%d.log", pthread_self());
+    FILE* f = fopen(filename, "a+");
+    if(f) {
+        char message[2000];
+        sprintf(message, "trace %s %d\n", function, trace_call++);
+        fwrite(message, strlen(message), 1, f);
+        std::fclose(f);
+    }
+}
--- src/trace-log.h
+++ src/trace-log.h
@@ -3,11 +3,14 @@
 //#include "loguru.hpp"
 
 #include "util.h"
+#include <pthread.h>
+#include <stdio.h>
 
 extern int trace_call;
 
-//#define FUNC_LOG_TRACE() LOG_F(INFO, "I'm hungry for some %.3f!", 3.14159);
-#define FUNC_LOG_TRACE() LogPrintf("trace %S %d\n", __FUNCTION__, trace_call++)
-//std::string teststr = "hjyvgfjhvyj";
-//#define FUNC_LOG_TRACE LogPrintStr(teststr);
-//#define FUNC_LOG_TRACE ;
+
+//#define FUNC_LOG_TRACE() LogPrintf("trace %S %d\n", __FUNCTION__, trace_call++)
+
+void WriteToFile(const char* function);
+
+#define FUNC_LOG_TRACE() WriteToFile(__PRETTY_FUNCTION__)

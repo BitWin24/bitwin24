b8d7326477a277140210c2fe9f98f37611d3edde
Svyatoslav Delukin <svyatoslav.d@blaize.tech>
2019-11-27 09:36:52 +0200

    add time to log

--- src/trace-log.cpp
+++ src/trace-log.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "trace-log.h"
+#include <ctime>
 
 
 int trace_call = 0;
@@ -11,14 +12,18 @@ int trace_call = 0;
 //loguru::add_file("trace_log.log", loguru::Append, loguru::Verbosity_MAX);
 
 
-void WriteToFile(const char* function)
-{
+void WriteToFile(const char *function) {
     char filename[256];
-    sprintf(filename, "/home/s/my%d.log", pthread_self());
-    FILE* f = fopen(filename, "a+");
-    if(f) {
+    sprintf(filename, "thread_%d.log", pthread_self());
+    FILE *f = fopen(filename, "a+");
+    if (f) {
         char message[2000];
-        sprintf(message, "trace %s %d\n", function, trace_call++);
+        time_t now = time(0);
+        struct tm *timeinfo;
+        timeinfo = localtime(&now);
+        char timeBuf[256];
+        strftime(timeBuf, sizeof(timeBuf), "%H:%M:%S", timeinfo);
+        sprintf(message, "%s: %s %d\n", timeBuf, function, trace_call++);
         fwrite(message, strlen(message), 1, f);
         std::fclose(f);
     }

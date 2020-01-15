35183092adab00d6c03bb6b8d67c959f8e79985b
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2020-01-15 10:41:53 +0100

    Update bitwin24_autoinstall.sh

--- bitwin24_autoinstall.sh
+++ bitwin24_autoinstall.sh
@@ -227,7 +227,7 @@ done
     #Adding bootstrap files 
 
 cd ~/.bitwin24/ && rm -rf blocks chainstate sporks zerocoin
-cd ~/.bitwin24/ && wget https://github.com/BitWin24/guides/blob/master/BITWIN24-bootstrap15012020.zip
+cd ~/.bitwin24/ && wget https://github.com/BitWin24/guides/raw/master/BITWIN24-bootstrap15012020.zip
 cd ~/.bitwin24/ && unzip BITWIN24-bootstrap15012020.zip
 
 # Create cryptoverification.conf

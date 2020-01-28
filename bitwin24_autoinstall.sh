12dee372cf67da457e83efc8d3507a0832aaa779
mrx0rhk <57437329+mrx0rhk@users.noreply.github.com>
2020-01-28 13:05:23 +0100

    Update bitwin24_autoinstall.sh

--- bitwin24_autoinstall.sh
+++ bitwin24_autoinstall.sh
@@ -224,9 +224,9 @@ done
     bitwin24-cli stop
     sleep 5
     
-    #Adding bootstrap files 
+#Adding bootstrap files 
 
-cd ~/.bitwin24/ && rm -rf blocks chainstate sporks zerocoin
+cd ~/.bitwin24/ && rm -rf blocks chainstate sporks zerocoin peers.dat
 cd ~/.bitwin24/ && wget https://github.com/BitWin24/guides/raw/master/bootstrap.zip
 cd ~/.bitwin24/ && unzip bootstrap.zip
 
